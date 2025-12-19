d90ce9a79e7efcb159759ad0432b24e12184ba54
saloni mehra <salonimehra2911@gmail.com>
2025-12-19 20:09:39 +0530

    Initial commit

--- app/src/main/java/com/example/college_project/MainActivity.java
+++ app/src/main/java/com/example/college_project/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.college_project;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout llMain;
+    ImageView ivMainLogo;
+    TextView tvMainTitle, tvMainSlogan;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1001);
+            }
+        }
+
+
+
+        llMain = findViewById(R.id.llMain);
+        ivMainLogo = findViewById(R.id.ivMainLogo);
+        tvMainTitle = findViewById(R.id.tvMainTitle);
+        tvMainSlogan = findViewById(R.id.tvMainSlogan);
     }
 }
+
